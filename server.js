eb77704747a485a1dc8a3557b411d35d408422c3
Nguyen Trung Chinh <nguyentrungchinh@Nguyens-MacBook-Pro.local>
2023-10-21 17:06:36 +0700

    second commit

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
 
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv')
-dotenv.config()
-
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 const cors = require('cors');
+dotenv.config();
 
-
-
-const app = express()
-const port = process.env.PORT || 10000
-
-
-
-// const buildPath = path.join(__dirname, 'build')
-
-buildPath = path.join('./', 'build')
-
-
-
+const app = express();
+const port = process.env.PORT || 3000;
+const buildPath = path.join('./', 'build');
 
 app.use(express.static(buildPath))
 app.use(express.json())
 app.use(cors())
 
-
 // gets the static files from the build folder
 app.get('*', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'))
 })
-
 
 app.post("/upload", (req, res) => {
     setTimeout(() => {
@@ -43,9 +30,7 @@ app.delete("/upload", (req, res) => {
     return res.status(200).json({ result: true, msg: 'file deleted' });
 });
 
-
 // Showing that the server is online and running and listening for changes
 app.listen(port, () => {
   console.log(`Server is online on port: ${port}`)
 })
-
